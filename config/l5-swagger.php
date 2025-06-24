777826d5d88a5e01bbb30f1af128ec31bd811f26
Muhammad Farhan <muhfarhanhidayatulloh@gmail.com>
2025-06-24 17:04:13 +0700

    Enable automatic documentation regeneration and proxy support in Swagger configuration

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -235,7 +235,7 @@ return [
          * Set this to `true` in development mode so that docs would be regenerated on each request
          * Set this to `false` to disable swagger generation on production
          */
-        'generate_always' => env('L5_SWAGGER_GENERATE_ALWAYS', false),
+        'generate_always' => env('L5_SWAGGER_GENERATE_ALWAYS', true),
 
         /*
          * Set this to `true` to generate a copy of documentation in yaml format
@@ -246,7 +246,7 @@ return [
          * Edit to trust the proxy's ip address - needed for AWS Load Balancer
          * string[]
          */
-        'proxy' => false,
+        'proxy' => true,
 
         /*
          * Configs plugin allows to fetch external configs instead of passing them to SwaggerUIBundle.
